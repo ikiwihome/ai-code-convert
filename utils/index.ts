ea4cd040b9aae34192b79c09c8263ea689795941
ikiwi <ikiwicc@gmail.com>
2023-08-19 13:35:28 +0800

    update new env

--- utils/index.ts
+++ utils/index.ts
@@ -87,24 +87,12 @@ export const OpenAIStream = async (
   const prompt = createPrompt(inputLanguage, outputLanguage, inputCode);
 
   const system = { role: 'system', content: prompt };
-	
-  // use openai 
-  // const url = "https://api.openai.com/v1/chat/completions";
-  // const key = process.env.NEXT_PUBLIC_OPENAI_API_KEY;
-  // const model = "gpt-3.5-turbo-16k";
-  // use cloudflare proxy, please create by yourself
-  const url = process.env.NEXT_PUBLIC_OPENAI_API_BASE_URL || 'https://openai-2zn.pages.dev/api/v1/chat/completions';
-  const key = process.env.NEXT_PUBLIC_OPENAI_API_KEY || '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';
-  const model = process.env.NEXT_PUBLIC_OPENAI_MODEL || 'gpt-3.5-turbo';
-  // use fake
-  // const url = "https://ai.fakeopen.com/v1/chat/completions";
-  // const key = "pk-this-is-a-real-free-pool-token-for-everyone";
-  // const model = "gpt-4";
-  // use api2d
-  // const url = "https://oa.api2d.net/v1/chat/completions";
-  // const key = process.env.NEXT_PUBLIC_OPENAI_API_KEY;
-  // const model = "gpt-3.5-turbo-16k";
-  
+
+
+  const url = `${process.env.NEXT_PUBLIC_OPENAI_API_BASE_URL}`
+  const key = process.env.NEXT_PUBLIC_OPENAI_API_KEY;
+  const model = process.env.NEXT_PUBLIC_OPENAI_MODEL;
+
   const res = await fetch(url, {
     headers: {
       'Content-Type': 'application/json',
